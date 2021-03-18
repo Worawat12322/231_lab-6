faeac7ba98b7f89e47914671474ab6c60bf07bf0
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-03-18 15:12:21 +0700

    update

--- sort.cpp
+++ sort.cpp
@@ -1,7 +1,24 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
-	int cnt;
+	int cnt,i,l,k;
 	scanf("%d",&cnt);
-	
+	char arr[cnt][15],rcv[15];
+	for(i=0;i<cnt;i++)
+	{
+		gets(rcv);
+		for(l=0;l<15;l++)
+		{
+			arr[i][l]=rcv[l];
+		}
+	}
+	for(i=0;i<cnt;i++)
+	{
+		for(l=0;;l++)
+		{
+			printf("%c",arr[i][l]);
+		}
+		l=0;
+	}
 	return 0;
 }
