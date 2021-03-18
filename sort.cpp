85db66cd19fc9aff2110d59998b2d264ea4385da
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-03-18 17:52:22 +0700

    update

--- sort.cpp
+++ sort.cpp
@@ -4,15 +4,16 @@ int main(){
 	int cnt,i,l,k;
 	scanf("%d",&cnt);
 	char arr[cnt][15],rcv[15];
+	fflush(stdin);
 	for(i=0;i<cnt;i++)
 	{
 		gets(rcv);
-		k=strlen(rcv);
-		for(l=0;l<=k;l++)
-		{
-			arr[i][l]=rcv[l];
-		}
+		strcpy(arr[i],rcv);
 	}
-	
+	int end;
+	for(end=0;end<cnt;end++)
+	{
+		puts(arr[end]);
+	}
 	return 0;
 }
