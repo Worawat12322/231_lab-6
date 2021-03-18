5034d4f1e0557010aa0e3c47633f336254a24d90
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-03-18 16:30:37 +0700

    update

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -1,12 +1,34 @@
 #include<stdio.h>
 int main(){
 	int n,q;
-	int i,l
+	int i,l,k;
+	int plus=0;
 	scanf("%d %d",&n,&q);
+	int st[n]={0};
+	char fst[q];
+	int snd[q][2];
 	if(n<=1&&q<=262144)
 	{
-		char st[n]={0};
-		for()
+		for(i=0;i<q;i++)
+		{
+			fflush(stdin);
+			scanf("%c %d %d",fst[i],snd[i][0],snd[i][1]);
+		}
+		for(l=0;l<q;l++)
+		{
+			if(fst[l]=='U')
+			{
+				st[snd[l][0]-1]=snd[l][1];
+			}
+			else if(fst[l]=='P')
+			{
+				for(k=snd[l][0]-1;k<=snd[l][1];k++)
+				{
+					plus=plus+st[k];	
+				}
+				printf("%d\n",plus);
+			}
+		}
 	}
 	return 0;
 }
