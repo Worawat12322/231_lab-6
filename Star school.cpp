9fcda0c5caaaa652db50a1776849f55cec6c1846
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-03-18 17:35:55 +0700

    update

--- Star school.cpp
+++ Star school.cpp
@@ -4,12 +4,32 @@ int main(){
 	scanf("%d",&a);
 	int score[a+1]={0};
 	int std[a+1][2]={0},i;
+	if(a>=2&&a<=1000)
+	{
+	
 	for(i=1;i<a+1;i++)
 	{
 		scanf("%d %d",&std[i][0],&std[i][1]);
 		score[std[i][0]]++;
 		score[std[i][1]]++;
 	}
+	int max=score[1],min=0,c,lo_max=1,lo_min;
+	for(c=1;c<a;c++)
+	{
+		if(max<score[c+1])
+		{
+			lo_max=c+1;
+		}
+	}
+	min=score[std[lo_max][0]];
+	lo_min=std[lo_max][0];
+	if(min>score[std[lo_max][1]])
+	{
+		lo_min=std[lo_max][1];
+	}
+	printf("%d %d",lo_max,lo_min);
 	
+	}
 	return 0;
 }
+
